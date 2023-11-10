3035ef30d5290a676a6aca05f1885c0dbadba66b
plasti <zachk3yata2@gmail.com>
2023-11-10 15:47:41 -0500

    added inactive liking action

--- dyna/newForumPost.js
+++ dyna/newForumPost.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 let newForumPost = async (raw, post_number, errors_permitted_count_left = 2) => {
+	// create new post
 	await axios(`https://forum.gethopscotch.com/posts.json`, {
 		method: 'POST',
 		headers: {
@@ -22,6 +23,30 @@ let newForumPost = async (raw, post_number, errors_permitted_count_left = 2) => 
 			console.log(error.response.data);
 			if (errors_permitted_count_left > 0) setTimeout(async () => await newForumPost(raw, post_number, errors_permitted_count_left - 1), 5000);
 		});
+
+	// like the user [NOT SET TO ACTIVE UNTIL API KEY PERMISSIONS GET UPDATED]
+	/*
+	await axios(`https://forum.gethopscotch.com/post_actions.json`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Api-Username': 'FarmBot',
+			'Api-Key': process.env.FBK,
+		},
+		data: {
+			id: post_number,
+			post_action_type_id: 0,
+			flag_topic: false,
+		},
+		redirect: 'follow',
+	})
+		.then((response) => response.data)
+		.then((result) => console.log(result))
+		.catch((error) => {
+			console.log('error', error);
+			console.log(error.response.data);
+		});
+	*/
 };
 
 module.exports.newForumPost = async function (raw, post_number) {
