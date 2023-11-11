a1842df3a39ea37a04dffc2e09c560f1b994f381
plasti <zachk3yata2@gmail.com>
2023-11-10 20:55:56 -0500

    fixed drop chances

--- commands/shop.js
+++ commands/shop.js
@@ -24,21 +24,27 @@ let cm = async (sentence, userDb) => {
 				for (const item of mooseFarms) {
 					existingPacks.push(item.packName);
 				}
-				console.log(existingPacks);
 				if (!existingPacks.includes(sentence[2])) {
 					return [`Invalid pack name \`${sentence[2]}\`. Reply with \`@FarmBot shop list\` for a detailed list of available products.`, userDb];
 				} else {
 					const targetPack = mooseFarms[existingPacks.indexOf(sentence[2])];
 					if (userDb.coins >= targetPack.packPrice) {
-						const randomSeedHT = Math.floor(Math.random() * targetPack.packContent.length);
-						const randomSeed = targetPack.packContent[randomSeedHT];
+						const randomNumb = Math.floor(Math.random() * 100);
+						let luckCount = 0;
+						let i = 0;
+						for (const seed of targetPack.packContent) {
+							if (seed.luck + luckCount > randomNumb) break;
+							luckCount += seed.luck;
+							i++;
+						}
+						const randomSeed = targetPack.packContent[i];
 
 						const isDouble = Math.floor(Math.random() * 10) == 0 && randomSeed.luck >= 15;
 
 						userDb.coins -= targetPack.packPrice;
 						userDb.seedsInventory[randomSeed.seedName + 'Seeds'] += isDouble + 1;
 
-						if (isDouble || true){
+						if (isDouble || true) {
 							return [`You bought one \`${targetPack.packName}\`, and... drum roll please! \n\n\u2757\u2757\u2757 \n\nYou opened your package and found **[u]TWO[/u] [spoiler]${randomSeed.seedName} seeds[/spoiler]**, both with a **${randomSeed.luck}% drop chance**!! The duplicate has a **10% chance** of happening. Wow, congrats!`, userDb];
 						} else {
 							return [`You bought one \`${targetPack.packName}\`, and... drum roll please! \n\n\uD83E\uDD41\uD83E\uDD41 \n\nYou opened your package and found **one [spoiler]${randomSeed.seedName} seed[/spoiler]**, with a **${randomSeed.luck}% drop chance**!!`, userDb];
