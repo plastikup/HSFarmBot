c45d8544b3de765dce6248492598fa2b183add1c
plasti <zachk3yata2@gmail.com>
2023-11-13 20:34:06 -0500

    terminology change

--- commands/auction.js
+++ commands/auction.js
@@ -5,7 +5,7 @@ const undoCamelCase = require('../scripts/undoCamelCase.js');
 const formatCountdown = require('../scripts/formatCountdown.js');
 
 let cm = async (sentence, userDb) => {
-	if (sentence[1] == undefined) return [`Valid subcommands for \`@FarmBot auction\`:\n- \`help\`: What's an auction, what's a bid? Everything you need to know before getting started;\n- \`info\`: view the current ongoing auction happening;\n- \`bid [amount]\`: bid an amount of coins on the ongoing auction.`, userDb];
+	if (sentence[1] == undefined) return [`Valid subcommands for \`@FarmBot auction\`:\n- \`help\`: What's an auction, what's a bid? Everything you need to know before getting started;\n- \`view\`: view the current ongoing auction happening;\n- \`bid [amount]\`: bid an amount of coins on the ongoing auction.`, userDb];
 	else {
 		let auction = await dbAu.get();
 
@@ -18,7 +18,7 @@ let cm = async (sentence, userDb) => {
 			case 'help':
 				return [auctionHelp, userDb];
 				break;
-			case 'info':
+			case 'view':
 				if (!baseAcc._active) return [`There is no ongoing auction right now. Check it again in a day or two!`, userDb];
 
 				let yourBidText = 'You have not bidden anything yet.';
@@ -27,7 +27,7 @@ let cm = async (sentence, userDb) => {
 					else yourBidText = `You have bidden ${auction[auction.findIndex((e) => e.username == userDb.username)].bidAmount} coins. It's not enough to win the auction!`;
 				}
 
-				return [`### Status of current ongoing auction\n> \uD83D\uDD0D **Bidding subject**: ${undoCamelCase.cm(baseAcc.bidSubject.subject)}\n> \uD83D\uDCB0 **Highest bid so far**: ${baseAcc.highestBid.amount} by ${baseAcc.highestBid.username}\n> \u23F3 **Ends in**: ${formatCountdown.cm(baseAcc.endsAt + 82800000)}\n\n${yourBidText}`, userDb];
+				return [`### Status of current ongoing auction\n> \uD83D\uDD0D **Bidding subject**: ${undoCamelCase.cm(baseAcc.bidSubject.subject)}\n> \uD83D\uDCB0 **Highest bid so far**: ${baseAcc.highestBid.amount} coins (by ${baseAcc.highestBid.username})\n> \u23F3 **Ends in**: ${formatCountdown.cm(baseAcc.endsAt + 82800000)}\n\n${yourBidText}`, userDb];
 				break;
 			case 'bid':
 				return [`[bidding coming soon]`, userDb];
