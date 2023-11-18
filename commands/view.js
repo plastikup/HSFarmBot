a91ae956474d4b102543f5ed667c52265b8e096f
plasti <zachk3yata2@gmail.com>
2023-11-18 11:56:21 -0500

    new inventory table formatting

--- commands/view.js
+++ commands/view.js
@@ -2,18 +2,20 @@ const generateFarmImg = require('../scripts/generateFarmImg.js');
 const undoCamelCase = require('../scripts/undoCamelCase.js');
 
 function inventoryContent(userDb) {
-	let inventoryContent = userDb.seedsInventory;
-	inventoryContent = Object.fromEntries(Object.entries(inventoryContent).sort(([, a], [, b]) => b - a));
-	let table = 'Seeds|Quantity\n-|-\n';
-	for (let item in inventoryContent) {
-		table = table + `${undoCamelCase.cm(item)}|${inventoryContent[item]}\n`;
-	}
+	let seedInventoryContent = userDb.seedsInventory;
+	seedInventoryContent = Object.fromEntries(Object.entries(seedInventoryContent).sort(([, a], [, b]) => b - a));
+	let cropInventoryContent = userDb.cropsInventory;
+	cropInventoryContent = Object.fromEntries(Object.entries(cropInventoryContent).sort(([, a], [, b]) => b - a));
 
-	inventoryContent = userDb.cropsInventory;
-	inventoryContent = Object.fromEntries(Object.entries(inventoryContent).sort(([, a], [, b]) => b - a));
-	table = table + '\nCrops|Quantity\n-|-\n';
-	for (let item in inventoryContent) {
-		table = table + `${undoCamelCase.cm(item)}|${inventoryContent[item]}\n`;
+	let table = 'Seeds|Quantity|&ic; &ic; &ic; &ic; &ic; &ic; &ic;|Crops|Quantity\n-|-|-|-|-\n';
+	for (let i = 0; i < Math.max(Object.keys(seedInventoryContent).length, Object.keys(cropInventoryContent).length); i++) {
+		const seed = Object.keys(seedInventoryContent)[i] || '-';
+		const crop = Object.keys(cropInventoryContent)[i] || '-';
+		let seedKey = undoCamelCase.cm(seed);
+		if (seed != '-') seedKey = '**' + seedKey.slice(0, seedKey.length - 6) + '** seeds';
+		let cropKey = undoCamelCase.cm(crop);
+		if (crop != '-') cropKey = '**' + cropKey.slice(0, cropKey.length - 6) + '** crops';
+		table = table + `${seedKey}|${seedInventoryContent[seed] || '-'}||${cropKey}|${cropInventoryContent[crop] || '-'}\n`;
 	}
 
 	/* not devlp yet
@@ -23,7 +25,7 @@ function inventoryContent(userDb) {
         for (let item in inventoryContent) {
             table = table + `${undoCamelCase.cm(item)}|${inventoryContent[item]}\n`;
         }
-        */
+    */
 
 	return table;
 }
