460a43229e84a974b8c54ff5a9094ca9cffb58db
plasti <zachk3yata2@gmail.com>
2023-11-18 10:25:36 -0500

    added 2% lilyValley drop rate on !daily

--- commands/daily.js
+++ commands/daily.js
@@ -3,11 +3,18 @@ const formatCountdown = require('../scripts/formatCountdown.js');
 let cm = async (userDb, devforced) => {
 	let lastDaily = userDb.lastDaily;
 	if (Date.now() > lastDaily + 82800000 || devforced) {
-		const randomGrant = Math.round(Math.random() * 100 + 50);
 		userDb.lastDaily = Date.now();
-		userDb.coins += randomGrant;
+		if (Math.random() >= 0.98){
+			// 2% chance of getting a lilyValley drop
+			userDb.seedsInventory.lilyValleySeeds += 1;
 
-		return [`Granted... &#x1f3b2; &#x1f3b2; **${randomGrant} coins** to your account! Come back in **23 hours** to request another \`@FarmBot daily\`!`, userDb];
+			return [`Granted... \u2757\u2757 **1 Lily Valley seed** to your account!! This drop has a 2% drop chance.\n\nCome back in **23 hours** to request another \`@FarmBot daily\`!`, userDb];
+		} else {
+			const randomGrant = Math.round(Math.random() * 100 + 50);
+			userDb.coins += randomGrant;
+	
+			return [`Granted... \uD83E\uDD41\uD83E\uDD41 **${randomGrant} coins** to your account! Come back in **23 hours** to request another \`@FarmBot daily\`!`, userDb];
+		}
 	} else {
 		return [`You have recently requested a **daily**. Try again in **${formatCountdown.cm(lastDaily + 82800000)}**!`, userDb];
 	}
