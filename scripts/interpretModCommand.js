2a2733777e64cf92d8673db224d0d4d431157b25
plasti <113269807+plastikup@users.noreply.github.com>
2024-01-08 18:32:10 -0500

    Update interpretModCommand.js

--- scripts/interpretModCommand.js
+++ scripts/interpretModCommand.js
@@ -6,34 +6,8 @@ const auctionFormatting = require('../scripts/auctionFormatting.js');
 module.exports = async (sentence, userDb, db) => {
 	let tgUserDb = null;
 	switch (sentence[1]) {
-		case 'test':
-			return await require('./interpretCommand.js').cm(test, userDb, false);
-			break;
-		case 'grant_s':
-			tgUserDb = require('../scripts/searchForAccount')(sentence[2], db);
-			if (tgUserDb == null) {
-				return `Unauthenticated user @${sentence[2]}.`;
-			} else {
-				for (let i = 3; i < sentence.length; i++) {
-					const types = ['blueberrySeeds', 'pinkTulipSeeds', 'strawberrySeeds', 'sunflowerSeeds', 'wheatSeeds'];
-					tgUserDb.seedsInventory[types[i - 3]] += Math.round(sentence[i]);
-				}
-				dbFus.put(tgUserDb._id, tgUserDb);
-				return `Granted specified seeds to @${sentence[2]}.`;
-			}
-			break;
-		case 'grant_c':
-			tgUserDb = require('../scripts/searchForAccount')(sentence[2], db);
-			if (tgUserDb == null) {
-				return `Unauthenticated user @${sentence[2]}.`;
-			} else {
-				for (let i = 3; i < sentence.length; i++) {
-					const types = ['blueberryCrops', 'pinkTulipCrops', 'strawberryCrops', 'sunflowerCrops', 'wheatCrops'];
-					tgUserDb.cropsInventory[types[i - 3]] += Math.round(sentence[i]);
-				}
-				dbFus.put(tgUserDb._id, tgUserDb);
-				return `Granted specified crops to @${sentence[2]}.`;
-			}
+		case 'version':
+			return 'The active version of the bot is currently v2024.0b.';
 			break;
 		case 'start_auction':
 			await dbAu.put(process.env.baseID, {
