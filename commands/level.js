377b35e59da37ed912c8f30dd5e23dfe9cfcdfbb
plasti <zachk3yata2@gmail.com>
2024-01-10 15:33:31 -0500

    slower level progression

--- commands/level.js
+++ commands/level.js
@@ -1,5 +1,5 @@
 module.exports = async (userDb) => {
-	const userLevel = Math.floor(Math.sqrt(+userDb.experiences + 1));
-	const requiredPointsForNextLevel = Math.round((userLevel + 1) ** 2 - 1) - +userDb.experiences;
+	const userLevel = Math.floor(0.25 * Math.sqrt(+userDb.experiences + 1));
+	const requiredPointsForNextLevel = Math.round(16 * (userLevel + 1) ** 2) - +userDb.experiences;
 	return `You are at **level ${userLevel}** with ${userDb.experiences} experience-points - you're **missing ${requiredPointsForNextLevel} points to reach level ${userLevel + 1}**!`;
 };
